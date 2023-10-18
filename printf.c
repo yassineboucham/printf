c75c2fa2ff73adbc937d7a441313226c32f6a7ed
root <root@DESKTOP-GSM3MPE.localdomain>
2023-10-18 01:33:09 +0100

    printf

--- printf.c
+++ printf.c
@@ -12,12 +12,15 @@ int _printf(const char *format, ...)
 	fbyf type[] = {
 		{'c', _putc},
 	};
-
+	if (format == NULL)
+	return (-1);
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
+			i++;
+			while (format[i] == ' ')
 			i++;
 			j = 0;
 			while (j < 1)
@@ -27,12 +30,15 @@ int _printf(const char *format, ...)
 					s += type[j].f(ap);
 					i++;
 				}
+				else
+					s += _putchar('%');
 				j++;
 			}
+			i--;
 		}
 		else
 		{
-		s += _putchar(format[i]);
+			s += _putchar(format[i]);
 		}
 		i++;
 	}
